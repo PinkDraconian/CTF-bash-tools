582cd562b0bb92d5686f706b7747eb79307a0bfe
PinkDraconian <vanroeyrobbe@hotmail.com>
2020-04-02 14:04:18 +0200

    Added ctf-connect alias to create a connect.sh script

--- make.sh
+++ make.sh
@@ -8,9 +8,11 @@
 
 echo "
 # Aliases for CTF-bash-tools (https://github.com/PinkDraconian/CTF-bash-tools)
-alias ctf-py=\"$(pwd)/scripts/ctf-py.sh\"" >> ~/.bash_aliases
+alias ctf-py=\"$(pwd)/scripts/ctf-py.sh\"
+alias ctf-connect=\"$(pwd)/scripts/ctf-connect.sh\"" >> ~/.bash_aliases
 
 # Prompt user to source ~/.bashrc or restart the terminal
 echo "Aliases have been added! You can now use
 	'ctf-py' to create a solve.py script
+	'ctf-connect ip port' to create a connect.sh script
 In order to use these, either run 'source ~/.bashrc' or restart the terminal"
