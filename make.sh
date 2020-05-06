ab758915e3cd1696720da9a5b7e0024c4892aa6c
PinkDraconian <vanroeyrobbe@hotmail.com>
2020-05-06 13:23:53 +0200

    Added ctf-ex script to make.sh

--- make.sh
+++ make.sh
@@ -9,11 +9,13 @@
 echo "
 # Aliases for CTF-bash-tools (https://github.com/PinkDraconian/CTF-bash-tools)
 alias ctf-py=\"$(pwd)/scripts/ctf-py.sh\"
-alias ctf-connect=\"$(pwd)/scripts/ctf-connect.sh\"" >> ~/.bash_aliases
+alias ctf-connect=\"$(pwd)/scripts/ctf-connect.sh\"
+alias ctf-ex=\"$(pwd)/scripts/ctf-ex.sh\"" >> ~/.bash_aliases
 
 # Prompt user to source ~/.bashrc or restart the terminal
 echo "Aliases have been added! You can now use
 	'ctf-py' to create a solve.py script
 	'ctf-connect ip port' to create a connect.sh script
+	'ctf-ex cmd' to see examples of a certain command
 In order to use these, either run 'source ~/.bashrc' or restart the terminal.
 If you upgraded the script, you can delete the old entries from ~/.bashrc"
