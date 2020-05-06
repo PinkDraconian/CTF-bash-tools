5e4dc7d190f2e428c3d32033d53fb9ef09e693e2
PinkDraconian <vanroeyrobbe@hotmail.com>
2020-05-06 13:47:48 +0200

    Updated makefile to include ssh script

--- make.sh
+++ make.sh
@@ -10,12 +10,14 @@ echo "
 # Aliases for CTF-bash-tools (https://github.com/PinkDraconian/CTF-bash-tools)
 alias ctf-py=\"$(pwd)/scripts/ctf-py.sh\"
 alias ctf-connect=\"$(pwd)/scripts/ctf-connect.sh\"
-alias ctf-ex=\"$(pwd)/scripts/ctf-ex.sh\"" >> ~/.bash_aliases
+alias ctf-ex=\"$(pwd)/scripts/ctf-ex.sh\"
+alias ctf-ssh=\"$(pwd)/scripts/ctf-ssh.sh\"" >> ~/.bash_aliases
 
 # Prompt user to source ~/.bashrc or restart the terminal
 echo "Aliases have been added! You can now use
 	'ctf-py' to create a solve.py script
 	'ctf-connect ip port' to create a connect.sh script
 	'ctf-ex cmd' to see examples of a certain command
+	'ctf-ssh' to generate an rsa keypair for ssh login
 In order to use these, either run 'source ~/.bashrc' or restart the terminal.
 If you upgraded the script, you can delete the old entries from ~/.bash_aliases"
